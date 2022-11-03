28ffc6d1e013f23d3c4f61b7ce523b3389b19073
Sander Brustad <sander.simonsen.brustad@gmail.com>
2022-11-03 19:02:00 +0100

    Add WIP of exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+let i = 0;
+let words = "";
 
+const repeatString = function(string, num) {
+for (i = 0; i < num; i++) {
+    words += string;
+    }
+    return words
 };
-
+repeatString();
 // Do not edit below this line
 module.exports = repeatString;
