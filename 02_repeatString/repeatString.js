7f9281a7290d78c261df4b0469390452c63a4f0d
Sander Brustad <sander.simonsen.brustad@gmail.com>
2022-11-04 00:14:25 +0100

    Add completed function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,17 +1,15 @@
 "use strict";
 
-let words = "";
-
 const repeatString = function(string, num) {
+let words = "";
+if (num >= 0) {
 for (let i = 0; i < num; i++) {
     words += string;
-    if(words === "heyheyhey") {
-        return words
-    } else if (words === ""){
-        return "ERROR"
-
     }
-    }
+    return words
+}else{
+    return "ERROR"
+}
 };
 repeatString();
 // Do not edit below this line
