0368415b64dc38ce9fc1f659ad6152ed63f80ff7
Sander Brustad <sander.simonsen.brustad@gmail.com>
2022-11-05 21:36:29 +0100

    Add back to scratch

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,9 @@
 const removeFromArray = function(values, remove) { 
-        let localArray
-    for (let i = 0; i < values.length; i++) {
-            if(values[i] === remove) {
-                    values.splice();
-                    localArray = values;
-            }
-            return localArray;
-    }   
- return localArray
+    let localArray;
+        for(let i = 0; i < values.length; i++) {
+        localArray = values;
+        }
+    return localArray
 };
 
 // Do not edit below this line
