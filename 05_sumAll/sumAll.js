352fee2174c119192df9482b5715927bf5bd1fa3
Sander Brustad <sander.simonsen.brustad@gmail.com>
2022-11-07 16:00:15 +0100

    Add going to work WIP

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(numberOne, numberTwo) {
+    let lowest = Math.min(numberOne, numberTwo);
+    let highest = Math.max(numberOne, numberTwo);
+    let numbers = [];
+    let finalSum;
+     for (let i = lowest; i <= highest; i++) {
+        numbers.push(i);
+      }
+      return finalSum;    
 };
-
 // Do not edit below this line
 module.exports = sumAll;
