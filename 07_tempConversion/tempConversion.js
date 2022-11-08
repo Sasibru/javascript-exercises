89d942c8e07d2177b530f20319fe24ab50a98b34
Sander Brustad <sander.simonsen.brustad@gmail.com>
2022-11-08 11:14:56 +0100

    Add working functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(input) {
+  let fahreinheit = input;
+  let toCelsius = (fahreinheit - 32) * 5 / 9;
+  let finalTemp = Math.round(toCelsius * 10) / 10
+  return finalTemp;;
 };
 
-const ctof = function() {
+const ctof = function(input) {
+  let celsius = input;
+  let toFahrenheit = celsius * 9 / 5 + 32;
+  let finalTemp = Math.round(toFahrenheit * 10) / 10;
+  return finalTemp;
 
 };
 
