8f7a4e71fc1bde0a96c00df663b0c8b39fceeabf
Sander Brustad <sander.simonsen.brustad@gmail.com>
2022-11-08 10:46:47 +0100

    Add working function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,10 +2,17 @@ const sumAll = function(numberOne, numberTwo) {
     let lowest = Math.min(numberOne, numberTwo);
     let highest = Math.max(numberOne, numberTwo);
     let numbers = [];
-    let finalSum;
+    let finalSum = 0;
      for (let i = lowest; i <= highest; i++) {
         numbers.push(i);
       }
+      if (lowest < -1 || typeof numberOne === "string" || typeof numberTwo === "string" || Array.isArray(numberOne) || Array.isArray(numberTwo)) {
+        return "ERROR";
+    } else {
+      for (let i = 0; i < numbers.length; i++) {
+        finalSum += numbers[i];
+      }
+    }
       return finalSum;    
 };
 // Do not edit below this line
