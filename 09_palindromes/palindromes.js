07b1a0b860e48fa694b7580b65a1c953a0a443c5
Sander Brustad <sander.simonsen.brustad@gmail.com>
2023-01-25 13:56:45 +0100

    Add WIP, fix so it works with multiple words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+        const removeExc = string.replaceAll("!", "");
+        const removePunct = removeExc.replaceAll(".", "");
+        const removeCharacters = removePunct;
+    const charactersRemoved = removeCharacters;
+    const lowerCase = charactersRemoved.toLowerCase();
+    const arrayValues = lowerCase.split("");
+    const reverseArrayValues = arrayValues.reverse();
+    const reverseString = reverseArrayValues.join("");
+    if (lowerCase === reverseString) {
+        return true;
+    }else {
+        return false;
+    };
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
